13431558451b40d11d0ce3672ce2ca6e018e92b8
Bobby <bwsfca@gmail.com>
2025-06-05 17:57:14 -0700

    Squashed commit of the following:
    commit cbec666df9fe37a264c715280baf704a39024136
    Author: Bobby <bwsfca@gmail.com>
    Date:   Thu Jun 5 17:56:52 2025 -0700
    
        Fix: dependcies issue.
    
    commit 9af25a2720b970fe277fae681b0d2d9c63d5ef3c
    Author: Bobby <bwsfca@gmail.com>
    Date:   Thu Jun 5 17:51:42 2025 -0700
    
        Fix: Changing the way I call pydantic_settings

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,8 +1,6 @@
 from app.repositories.em_repository import SqlEmRepository
 from app.services.em_service import EmService
 
-def get_repository():
-    return SqlEmRepository()
-
-def get_service(repo: SqlEmRepository = get_repository()):
+def get_service():
+    repo = SqlEmRepository()
     return EmService(repo)
