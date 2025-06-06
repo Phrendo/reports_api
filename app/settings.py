0d7b7cc9bba6f60a477c769994f458770f1d0945
Bobby <bwsfca@gmail.com>
2025-06-05 18:00:02 -0700

    Squashed commit of the following:
    commit a64e34393ce23548b01c7f2d7769d020f44139ca
    Author: Bobby <bwsfca@gmail.com>
    Date:   Thu Jun 5 17:58:32 2025 -0700
    
        Pydantic Settings changges
    
    commit cbec666df9fe37a264c715280baf704a39024136
    Author: Bobby <bwsfca@gmail.com>
    Date:   Thu Jun 5 17:56:52 2025 -0700
    
        Fix: dependcies issue.
    
    commit 9af25a2720b970fe277fae681b0d2d9c63d5ef3c
    Author: Bobby <bwsfca@gmail.com>
    Date:   Thu Jun 5 17:51:42 2025 -0700
    
        Fix: Changing the way I call pydantic_settings

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,4 @@
-# reports_api/app/settings.py
+# app/settings.py
 
 import os
 from pydantic_settings import BaseSettings
@@ -6,8 +6,14 @@ from typing import List
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    CORS_ORIGINS_RAW: str = ""
     CORS_ORIGINS: List[str] = []
 
     class Config:
         env_file = ".env"
         case_sensitive = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        raw = os.getenv("CORS_ORIGINS_RAW", "")
+        self.CORS_ORIGINS = [origin.strip() for origin in raw.split(",") if origin.strip()]
